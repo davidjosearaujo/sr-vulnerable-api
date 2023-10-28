0b89352aae82069dbecb6d8a4ee92da01ee8bb37
David Araújo <david2araujo5@gmail.com>
2023-10-28 16:02:03 +0100

    add queries to database

--- api/DB.js
+++ api/DB.js
@@ -1,65 +1,102 @@
 class DB {
-
-
     constructor() {
-        this.dbname = './database.db';
-        (async() => {
-          await this.reset();
-        })()
+        this.dbname = "./database.db";
+        (async () => {
+            await this.reset();
+        })();
     }
 
     async reset() {
-      const fs = require('fs')
-      
-      if(fs.existsSync(this.dbname)) {
-        fs.unlinkSync(this.dbname)
-      }
+        const fs = require("fs");
 
-      this.db = new (require('sqlite3').verbose()).Database(this.dbname, (err) => {
-        if(err) {
-          console.error(err.message)
-        } else {
-          console.log('Connected to the SQLite database');
-          this.runSchemaQueryFromFile('./tables.sql');
+        if (fs.existsSync(this.dbname)) {
+            fs.unlinkSync(this.dbname);
         }
-      });
+
+        this.db = new (require("sqlite3").verbose().Database)(
+            this.dbname,
+            (err) => {
+                if (err) {
+                    console.error(err.message);
+                } else {
+                    console.log("Connected to the SQLite database");
+                    this.runSchemaQueryFromFile("./tables.sql");
+                }
+            }
+        );
     }
 
     async runSchemaQueryFromFile(filePath) {
-        const fs = require('fs')
+        const fs = require("fs");
         return new Promise((resolve, reject) => {
-          fs.readFile(filePath, 'utf8', (err, schemaQuery) => {
-            if (err) {
-              reject(err);
-              return;
-            }
-    
-            this.db.exec(schemaQuery, (err) => {
-              if (err) {
-                reject(err);
-              } else {
-                resolve('Schema query executed successfully.');
-              }
+            fs.readFile(filePath, "utf8", (err, schemaQuery) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                this.db.exec(schemaQuery, (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve("Schema query executed successfully.");
+                    }
+                });
             });
-          });
         });
-      }
+    }
+
+    async sanatizedQuery(func, ...args) {
+        return func();
+    }
 
     async validUser(username, password) {
-        return this.db.all(`SELECT * FROM users WHERE username=${username} AND password=${password}`, 
-            (err,rows) => {
-                if(err)
-                    return false;
+        return this.db.all(
+            `SELECT * FROM users WHERE username=? AND password=?`,
+            [username, password],
+            (err, rows) => {
+                if (err) return false;
 
                 return rows.length > 0 ? true : false;
             }
-        )
+        );
     }
 
-    async getTeacherClasses() {
+    async getTeacherClasses(id) {
+        return this.db.all(
+            `SELECT * FROM classes WHERE teacher_id=?`,
+            [id],
+            (err, rows) => {
+                if (err) return false;
 
+                return rows.length > 0 ? true : false;
+            }
+        );
+    }
+
+    async getStudentClasses(id) {
+        return this.db.all(
+            `SELECT * FROM student_classes WHERE student_id=?`,
+            [id],
+            (err, rows) => {
+                if (err) return false;
+
+                return rows.length > 0 ? true : false;
+            }
+        );
+    }
+
+    async deleteStudentClass(class_id, student_id) {
+        return this.db.all(
+            "DELETE FROM student_classes WHERE student_id = ? AND class_id = ?",
+            [student_id, class_id],
+            (err, rows) => {
+                if (err) return false;
+
+                return rows.length > 0 ? true : false;
+            }
+        );
     }
 }
-
 
 module.exports = DB;
--- api/app.js
+++ api/app.js
@@ -10,7 +10,6 @@ app.post('/login', (req,res) => {
 
 })
 
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
