3d36126ccc4bed8aadec25a33a4a9c8b18e526f2
David Araújo <david2araujo5@gmail.com>
2023-10-30 11:48:41 +0000

    Queries added

--- api/DB.js
+++ api/DB.js
@@ -48,24 +48,52 @@ class DB {
 
     async getUser(username, password) {
         return new Promise((resolve, reject) => {
-          this.db.get(
-            'SELECT * FROM teachers WHERE username = ? AND password = ?',
-            [username, password],
-            (err, row) => {
-              if (err) {
-                reject(err);
-              } else {
-                resolve(row);
-              }
-            }
-          );
+            this.db.all(
+                "SELECT * FROM teachers WHERE username = ? AND password = ?",
+                [username, password],
+                (err, row) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(row);
+                    }
+                }
+            );
         });
     }
 
-    async getTeacherClasses() {
-
+    async getTeacherClasses(id) {
+        return this.db.all(
+            `SELECT * FROM classes WHERE teacher_id=?`,
+            [id],
+            (err, rows) => {
+                if (err) return false;
+                return rows.length > 0 ? true : false;
+            }
+        );
     }
 
-  }
+    async getStudentClasses(id) {
+        return this.db.all(
+            `SELECT * FROM student_classes WHERE student_id=?`,
+            [id],
+            (err, rows) => {
+                if (err) return false;
+                return rows.length > 0 ? true : false;
+            }
+        );
+    }
+
+    async deleteStudentClass(class_id, student_id) {
+        return this.db.all(
+            "DELETE FROM student_classes WHERE student_id = ? AND class_id = ?",
+            [student_id, class_id],
+            (err, rows) => {
+                if (err) return false;
+                return rows.length > 0 ? true : false;
+            }
+        );
+    }
+}
 
 module.exports = DB;
